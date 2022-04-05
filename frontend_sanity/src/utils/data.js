0a1b7540d4b9a5c9195347c70557d83e2772f806
nguyendong47 <dongnguyenvan.it@gmail.com>
2022-04-05 15:00:02 +0700

    first commit

---
+++ frontend_sanity/src/utils/data.js
@@ -0,0 +1,229 @@
+export const categories = [
+	{
+		name: 'Cổng trường',
+		image:
+			'https://scontent.fhan5-6.fna.fbcdn.net/v/t1.6435-9/152439654_3725959587490954_3636794407223556654_n.png?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_ohc=b-UpLTv5OF8AX_lMsQc&_nc_ht=scontent.fhan5-6.fna&oh=00_AT8u0JDLDvqkkMlv0KXfH8emh-2-w92qpTJoN-1ZOPoS1w&oe=626E5A68',
+	},
+	{
+		name: 'Khu A',
+		image:
+			'https://scontent.fhan5-10.fna.fbcdn.net/v/t1.6435-9/140422705_3648894511864129_5548189129851873002_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=lOZ9M5N4PacAX_Tti7W&_nc_ht=scontent.fhan5-10.fna&oh=00_AT8At-00oZ8lpPR8HlKKW9aA6upBADNo1OWNdqT3iEuuog&oe=626EEE86',
+	},
+	{
+		name: 'Khu B',
+		image:
+			'https://scontent.fhan5-11.fna.fbcdn.net/v/t1.6435-9/140280028_3648894795197434_8056134328114239303_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=uO6gI7yo3PIAX9UmZnV&_nc_ht=scontent.fhan5-11.fna&oh=00_AT9u4sNXPfUr6lhJzcAamh1TtES7hJSjKS5WbATk8bHpyQ&oe=626EC5B1',
+	},
+	{
+		name: 'Khu C',
+		image:
+			'https://scontent.fhan5-10.fna.fbcdn.net/v/t1.6435-9/140316895_3648894721864108_562660526206784995_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=730e14&_nc_ohc=TXTAErkjKd4AX_TBX4z&tn=h-5USVohhKGwOoy1&_nc_ht=scontent.fhan5-10.fna&oh=00_AT8PTIcOSChzh9clTCYDy6ZtYjuy7KQTFUDwt0uWYYY2SA&oe=626B86CC',
+	},
+	{
+		name: 'Thư viện',
+		image:
+			'https://scontent.fhan5-8.fna.fbcdn.net/v/t1.6435-9/141048144_3648894108530836_8674218857011071669_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=vJzI0yr4hPoAX9GMyxc&_nc_ht=scontent.fhan5-8.fna&oh=00_AT91iKHNowbNXN63fpuA9DvQM28ieMrRRBcSUgqhnZ1NmA&oe=626DF61D',
+	},
+	{
+		name: 'Giảng đường',
+		image:
+			'https://scontent.fhan5-8.fna.fbcdn.net/v/t1.6435-9/141027179_3648894385197475_5554727273033261673_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=WCpNgWY6B-MAX_GmoaM&_nc_ht=scontent.fhan5-8.fna&oh=00_AT_I8Bf9mxSJVhVijUR9VS7ZnU72nj-YD_1bUYJv-DahkA&oe=626F0915',
+	},
+	{
+		name: 'Vườn cây',
+		image:
+			'https://scontent.fhan5-8.fna.fbcdn.net/v/t1.6435-9/192051163_4010903862329857_7410185676707793060_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=730e14&_nc_ohc=sJXDnVxwCL0AX-6Szjq&_nc_ht=scontent.fhan5-8.fna&oh=00_AT8YVasVXdJn6OLzMk8zbBz8QTtKKQNn-s6zJx9jDpTINg&oe=626D4B32',
+	},
+	{
+		name: 'Sân vận động',
+		image:
+			'https://scontent.fhan5-6.fna.fbcdn.net/v/t1.6435-9/194808263_4053588178061425_6061119646793868316_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=NW3_ovsKPb8AX-5XVnF&_nc_ht=scontent.fhan5-6.fna&oh=00_AT8wUMxRAJwF8ltGAL1aNppfm_wZItb4xPkhow4N3jkAmQ&oe=626C82CF',
+	},
+	{
+		name: 'Ký túc xá',
+		image:
+			'https://scontent.fhan5-2.fna.fbcdn.net/v/t1.6435-9/200451497_4053588311394745_4998712977664356750_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=g0cAUd-6rGMAX8txF6y&_nc_ht=scontent.fhan5-2.fna&oh=00_AT97Lh4R6gRdyoSzb7XuP-NHhlg_Mhiu1juKoUwp4m8txw&oe=626ED969',
+	},
+	{
+		name: 'Hoạt động',
+		image:
+			'https://scontent.fhan5-8.fna.fbcdn.net/v/t1.6435-9/160007350_3781686628584916_6222536907874413699_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=e3f864&_nc_ohc=8ucEytdvXhUAX80dd5P&_nc_ht=scontent.fhan5-8.fna&oh=00_AT_g1viRzMjOlHYsK8oajn2jTYhuN2QW-a-f9EUmfHPB0A&oe=626F2DFD',
+	},
+	{
+		name: 'Khu quân sự',
+		image:
+			'https://scontent.fhan5-7.fna.fbcdn.net/v/t39.30808-6/240594527_4374845122602394_5260116148413716615_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=_IyOkxC5784AX9lAulm&_nc_ht=scontent.fhan5-7.fna&oh=00_AT9z5t-Nl1NnJc5w91UkZqYdmyphxx6M3UaE2-ofPALRig&oe=624D6151',
+	},
+	{
+		name: 'Khác',
+		image:
+			'https://scontent.fhan5-7.fna.fbcdn.net/v/t1.6435-9/140305226_3648894431864137_5285681490567055633_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=730e14&_nc_ohc=RVKTtI4E8bEAX_qg0fB&_nc_ht=scontent.fhan5-7.fna&oh=00_AT-shF98Cdgf7Ug3KLR8F6P_K33lh0fheHkKvSFBOM8xrQ&oe=626C817B',
+	},
+];
+
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
+  image{
+    asset->{
+      url
+    }
+  },
+      _id,
+      destination,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    } `;
+
+export const pinDetailQuery = (pinId) => {
+	const query = `*[_type == "pin" && _id == '${pinId}']{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    title, 
+    about,
+    category,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+   save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+    comments[]{
+      comment,
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    }
+  }`;
+	return query;
+};
+
+export const pinDetailMorePinQuery = (pin) => {
+	const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+	return query;
+};
+
+export const searchQuery = (searchTerm) => {
+	const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+        image{
+          asset->{
+            url
+          }
+        },
+            _id,
+            destination,
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+            save[]{
+              _key,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+            },
+          }`;
+	return query;
+};
+
+export const userQuery = (userId) => {
+	const query = `*[_type == "user" && _id == '${userId}']`;
+	return query;
+};
+
+export const userCreatedPinsQuery = (userId) => {
+	const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+	return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+	const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+	return query;
+};
